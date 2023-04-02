382ba6282cb4388f7755d023c4f6b1d359fa16da
RomaFedoro <fedorovroman1506@gmail.com>
2023-04-02 13:30:16 +0700

    build: browserslist

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import svgr from 'vite-plugin-svgr';
 import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), legacy()],
   resolve: {
     alias: [
       {
