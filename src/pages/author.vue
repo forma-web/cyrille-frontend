0bc03acb1946adcc1d70a2d02973e006e8925eb8
DaVas255 <101551998+DaVas255@users.noreply.github.com>
2023-11-20 22:49:31 +0700

    rename from about to author

--- src/pages/author.vue
+++ src/pages/author.vue
@@ -1,36 +1,35 @@
-import { CyrBookCard } from '../../.nuxt/components';
 <template>
-  <div class="about-page">
-    <div class="about-page__author">
-      <h2 class="about-page__title">author</h2>
-      <CyrRhombus class="about-page__icon" />
+  <div class="author-page">
+    <div class="author-page__content">
+      <h2 class="author-page__title">author</h2>
+      <CyrRhombus class="author-page__icon" />
       <img
         src="/avatar/CyrilleDessal.png"
         alt="author-avatar"
-        class="about-page__author-avatar"
+        class="author-page__avatar"
       />
-      <p class="about-page__author-name">Cyrille D’essai</p>
+      <p class="author-page__name">Cyrille D’essai</p>
     </div>
 
-    <div class="about-page__author-info">
-      <div class="about-page__subtitle">About</div>
-      <p class="about-page__text-about">
+    <div class="author-page__info">
+      <div class="author-page__subtitle">author</div>
+      <p class="author-page__text">
         Cyrille D'essai is a talented male author known for his engaging and
         thought-provoking writing style. He has a way with words that captivates
         readers and draws them into his imaginative worlds. With a keen eye for
         detail and a gift for crafting complex characters, Cyrille's stories are
         both entertaining and insightful.
       </p>
-      <p class="about-page__text-about">
+      <p class="author-page__text">
         Although he is a relatively new author, he has already made a name for
         himself in the literary community and is highly respected by his peers.
         With his passion for storytelling and dedication to his craft, the
         author is poised to become a literary sensation.
       </p>
     </div>
-    <div class="about-page__author-books">
-      <div class="about-page__subtitle">Books</div>
-      <div class="about-page__cards">
+    <div class="author-page__books">
+      <div class="author-page__subtitle">Books</div>
+      <div class="author-page__cards">
         <CyrBookCard :card-type="'full'" />
         <CyrBookCard :card-type="'cover'" />
         <CyrBookCard :card-type="'table'" />
@@ -40,7 +39,7 @@ import { CyrBookCard } from '../../.nuxt/components';
 </template>
 
 <style lang="scss" scoped>
-.about-page {
+.author-page {
   display: grid;
   grid-template-columns: 1.5fr 2fr;
   grid-template-rows: 1fr 2fr;
@@ -49,7 +48,7 @@ import { CyrBookCard } from '../../.nuxt/components';
   background-color: #040404;
   color: white;
 
-  &__author {
+  &__content {
     grid-row: 1 / 3;
     padding: 5rem;
     display: flex;
@@ -68,18 +67,18 @@ import { CyrBookCard } from '../../.nuxt/components';
     margin-bottom: 57px;
   }
 
-  &__author-avatar {
+  &__avatar {
     margin-bottom: 35px;
   }
 
-  &__author-name {
+  &__name {
     font-family: Georgia, 'Times New Roman', Times, serif;
     font-size: 24px;
     font-weight: 400;
     font-style: italic;
   }
 
-  &__author-info {
+  &__info {
     padding: 3rem;
   }
 
@@ -90,14 +89,14 @@ import { CyrBookCard } from '../../.nuxt/components';
     text-transform: uppercase;
   }
 
-  &__text-about {
+  &__text {
     font-size: 16px;
     font-weight: 100;
     line-height: 19px;
     color: #d0d0d0;
   }
 
-  &__author-books {
+  &__books {
     padding: 3rem;
   }
 
