62ed57027b3296949ddb5a470bfd65a7354747b3
robonen <robonenandrew@gmail.com>
2023-11-23 00:15:14 +0700

    feat(ui): header components

---
+++ src/components/widgets/Header/AuthHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+</script>
+
+<template>
+  <BaseHeader>
+    <NuxtLink to="/" class="close-icon">
+      <XMarkIcon />
+    </NuxtLink>
+  </BaseHeader>
+</template>
+
+<style lang="scss" scoped>
+.close-icon {
+  width: 2.4rem;
+  height: 2.4rem;
+  color: white;
+}
+</style>
