b553e91e41996d03e8a97a2a28e76d307bd1b732
robonen <robonenandrew@gmail.com>
2023-11-24 20:30:23 +0700

    feat(ui): main page section, triggers demo

---
+++ src/components/widgets/Landing/HeadlessSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10rem;
+}
+</style>
