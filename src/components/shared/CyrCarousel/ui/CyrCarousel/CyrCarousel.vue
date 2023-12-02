0f4326f063852fbe630def71f3a2a7b456204bca
robonen <robonenandrew@gmail.com>
2023-12-03 03:22:53 +0700

    feat(ui): draft CyrCarousel

---
+++ src/components/shared/CyrCarousel/ui/CyrCarousel/CyrCarousel.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts" generic="T extends unknown">
+import { CyrCarouselItem } from '..';
+import style from './CyrCarousel.module.scss';
+
+interface CarouselProps {
+  items: T[];
+}
+
+const { items } = defineProps<CarouselProps>();
+
+defineSlots<{
+  default(props: { item: T }): unknown;
+}>();
+</script>
+
+<template>
+  <div :class="style.carousel">
+    <ul :class="style.carousel__list">
+      <!-- TODO: fix key -->
+      <CyrCarouselItem
+        v-for="(item, idx) in items"
+        :key="idx"
+        :class="style.carousel__item"
+      >
+        <slot :item="item" />
+      </CyrCarouselItem>
+    </ul>
+  </div>
+</template>
---
+++ src/components/shared/CyrCarousel/ui/CyrCarouselItem/CyrCarouselItem.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import style from './CyrCarouselItem.module.scss';
+</script>
+
+<template>
+  <li :class="style.item">
+    <slot />
+  </li>
+</template>
