a87652b166d39f16ed53fce86a4679440c71d206
robonen <robonenandrew@gmail.com>
2024-02-17 17:17:52 +0700

    feat: add checkbox component

--- src/shared/ui/Checkbox/Checkbox.vue
+++ src/shared/ui/Checkbox/Checkbox.vue
@@ -1,45 +1,16 @@
 <script setup lang="ts">
 import { CheckIcon } from '@heroicons/vue/24/solid';
-import style from './Checkbox.module.scss';
+import styles from './Checkbox.module.scss';
+
+const model = defineModel<boolean>();
 </script>
 
 <template>
-  <div :class="$style.checkbox">
-    <label :class="$style.checkbox__label">
-      <CheckIcon :class="$style.checkbox__icon" />
-      <input type="checkbox" :class="$style.checkbox__input" />
-    </label>
-  </div>
+  <label :class="styles.checkbox">
+    <input type="checkbox" :class="styles.checkbox__input" v-model="model" />
+    <span :class="styles.checkbox__box">
+      <CheckIcon :class="styles.checkbox__icon" />
+    </span>
+    <span :class="styles.checkbox__label"><slot /></span>
+  </label>
 </template>
-
-<style module lang="scss">
-.checkbox {
-  width: 2rem;
-  height: 2rem;
-  user-select: none;
-
-  &__label {
-    display: block;
-    width: 100%;
-    height: 100%;
-  }
-
-  &__icon {
-    width: 100%;
-    height: 100%;
-    padding: 0.3rem;
-    border: 1.5px solid var(--gray30);
-    border-radius: 0.4rem;
-  }
-
-  &__input {
-    position: absolute;
-    width: 0;
-    height: 0;
-  }
-
-  &__input:checked + &__icon {
-    background-color: var(--accent-color);
-  }
-}
-</style>
