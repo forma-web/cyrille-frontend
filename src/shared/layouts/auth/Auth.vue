82b76d2965aacb23cf1a5364df8a1663b31c3887
robonen <robonenandrew@gmail.com>
2024-02-17 17:16:01 +0700

    refactor: update auth page height

--- src/shared/layouts/auth/Auth.vue
+++ src/shared/layouts/auth/Auth.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { AuthHeader } from '@widgets/Header';
-import style from './Auth.module.scss';
+import styles from './Auth.module.scss';
 </script>
 
 <template>
-  <div :class="style.layout">
+  <div :class="styles.layout">
     <AuthHeader />
-    <main :class="style.layout__content">
-      <div :class="style.layout__slot">
+    <main :class="styles.layout__content">
+      <div :class="styles.layout__slot">
         <slot></slot>
       </div>
-      <div :class="style.layout__picture">
-        <div :class="style.layout__title">
+      <div :class="styles.layout__picture">
+        <div :class="styles.layout__title">
           Joining us means joining
           <span class="accent-font">the future reading</span> realm.
         </div>
