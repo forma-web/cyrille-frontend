e9691270a63de4226ee5e924077404cfe77b1eee
robonen <robonenandrew@gmail.com>
2024-02-17 17:23:40 +0700

    feat: library page stubs

--- src/pages/library.vue
+++ src/pages/library.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { CyrCheckbox } from '@shared/ui/Checkbox';
+import { CyrSwitch } from '@shared/ui/Switch';
+import { CyrRadioGroup, CyrRadio } from '@shared/ui/Radio';
+
+const selected = ref('date');
 </script>
 
 <template>
@@ -7,7 +11,24 @@ import { CyrCheckbox } from '@shared/ui/Checkbox';
     <h2 :class="$style.library__header">Library</h2>
     <div></div>
     <aside :class="$style.library__filters">
-      <CyrCheckbox />
+      <h5>FILTERS</h5>
+      <section>
+        <h5>LANGUAGE</h5>
+        <CyrCheckbox> Russian </CyrCheckbox>
+        <CyrCheckbox> English </CyrCheckbox>
+      </section>
+      <section>
+        <CyrSwitch> On sale </CyrSwitch>
+        <CyrSwitch> Highly rated </CyrSwitch>
+        <CyrSwitch> Exclusive </CyrSwitch>
+      </section>
+      <section>
+        <h5>SORT BY</h5>
+        <CyrRadioGroup v-model="selected">
+          <CyrRadio value="date"> Date </CyrRadio>
+          <CyrRadio value="rating"> Rating </CyrRadio>
+        </CyrRadioGroup>
+      </section>
     </aside>
     <NuxtPage />
   </div>
@@ -27,7 +48,30 @@ import { CyrCheckbox } from '@shared/ui/Checkbox';
   }
 
   &__filters {
+    position: sticky;
+    height: min-content;
+    padding: 2rem;
     border-right: 1.5px solid var(--gray30);
+
+    & > section {
+      display: flex;
+      flex-direction: column;
+      gap: 1.2rem;
+      margin-bottom: 2rem;
+      padding-bottom: 2rem;
+      border-bottom: 1px solid var(--gray30);
+
+      &:last-child {
+        border-bottom: none;
+      }
+
+      & > h5 {
+        margin: 0 0 1.2rem 0;
+        color: var(--gray50);
+        font-size: 1.2rem;
+        font-weight: 500;
+      }
+    }
   }
 }
 </style>
