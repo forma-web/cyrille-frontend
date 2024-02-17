dbd08c7e6ec78e99cd1834acbca21be239dbcfa8
robonen <robonenandrew@gmail.com>
2024-02-17 17:22:34 +0700

    refactor: fix imports

--- src/widgets/Footer/Footer.vue
+++ src/widgets/Footer/Footer.vue
@@ -3,30 +3,29 @@ import { CyrButton } from '@shared/ui/Button';
 import { CyrLogo } from '@shared/ui/Logo';
 import { CyrRhombus } from '@shared/ui/Rhombus';
 import { ScrollButton } from '@features/ScrollButton';
-
-import style from './Footer.module.scss';
+import styles from './Footer.module.scss';
 </script>
 
 <template>
-  <footer class="dark" :class="style.footer">
-    <div :class="style.footer__rhombus">
+  <footer class="dark" :class="styles.footer">
+    <div :class="styles.footer__rhombus">
       <CyrRhombus />
     </div>
-    <div :class="style.footer__container">
-      <CyrLogo :class="style.footer__logo" />
+    <div :class="styles.footer__container">
+      <CyrLogo :class="styles.footer__logo" />
       <ScrollButton
         direction="up"
         class="accent-font"
-        :class="style.footer__scroll"
+        :class="styles.footer__scroll"
       />
       <CyrButton
         variant="transparent"
         class="accent-font"
-        :class="style.footer__subscribe"
+        :class="styles.footer__subscribe"
       >
         Subscribe to newsletters
       </CyrButton>
-      <CyrButton variant="outline" :class="style.footer__app">
+      <CyrButton variant="outline" :class="styles.footer__app">
         Download app
       </CyrButton>
     </div>
