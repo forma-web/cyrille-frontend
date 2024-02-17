6931d3a52f2b737ed55bd01231f241a08f4381ae
robonen <robonenandrew@gmail.com>
2024-02-17 17:19:06 +0700

    feat: add radio component

---
+++ src/shared/ui/Radio/ui/Radio/Radio.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+interface RadioProps {
+  value: string;
+}
+</script>
+
+<script setup lang="ts">
+import { injectRadioGroupContext } from '../RadioGroup/RadioGroup.vue';
+import styles from './Radio.module.scss';
+
+const { value } = defineProps<RadioProps>();
+
+const context = injectRadioGroupContext();
+
+const checked = computed(() => context.modelValue?.value === value);
+</script>
+
+<template>
+  <label :class="styles.radio">
+    <input
+      type="radio"
+      :checked
+      :class="styles.radio__input"
+      :name="context.name"
+      @click="context.changeModelValue(value)"
+    />
+    <span :class="styles.radio__box">
+      <span :class="styles.radio__mark"></span>
+    </span>
+    <span :class="styles.radio__label"><slot /></span>
+  </label>
+</template>
